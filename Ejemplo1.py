cf4f2b0122c6f34a7cf786ec13f34204e8676113
AlexPortero24 <ad.porterod@uea.edu.ec>
2025-06-18 18:58:38 -0500

    En la Versión 3: Se agregó la clase Consulta y el historial de consultas para Mascota

--- Ejemplo1.py
+++ Ejemplo1.py
@@ -5,6 +5,7 @@
 # clinica_veterinaria_v1.py
 
 # Clase Dueño
+
 class Dueño:
     def __init__(self, nombre, telefono):
         self.nombre = nombre
@@ -24,6 +25,21 @@ class Mascota:
         print(f"Mascota: {self.nombre}, Especie: {self.especie}, Edad: {self.edad}")
         self.dueño.mostrar_info()
 
+    def agregar_consulta(self, motivo, fecha):
+        nueva = Consulta(self, motivo, fecha)
+        self.consultas.append(nueva)
+
+    def mostrar_historial(self):
+        print(f"Consultas de {self.nombre}:")
+        for consulta in self.consultas: 
+            print(f"- {consulta.motivo} el {consulta.fecha}")
+class Consulta:
+    def __init__(self, mascota, motivo, fecha):
+        self.mascota = mascota
+        self.motivo = motivo
+        self.fecha = fecha
+
+
 # Ejemplo de como se usa
 dueño1 = Dueño("Alex Portero", "0963002378")
 mascota1 = Mascota("Mia", "Gato", 4, dueño1)
@@ -32,4 +48,4 @@ print("Mascota:", mascota1.nombre)
 print("Dueño:", mascota1.dueño.nombre)
 
 mascota1.mostrar_info()
-
+mascota1.mostrar_historial()
