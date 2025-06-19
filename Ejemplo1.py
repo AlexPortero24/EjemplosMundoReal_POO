8bfb0bc4318e81c3ac92ec2eb60a749d8c24d5e1
AlexPortero24 <ad.porterod@uea.edu.ec>
2025-06-18 19:27:53 -0500

    se reviso el codigo y se identifico que el metodo de consultas no se definio, tambien se realizo mas explicaciones de las lineas de codigo. y se palnteo para poner una funcion que pida al usuario ingresar los datos , para mejorar la interaccion.

--- Ejemplo1.py
+++ Ejemplo1.py
@@ -25,30 +25,44 @@ class Mascota:
         self.consultas = []   # lista vacía para guardar sus consultas médicas
 
     def mostrar_info(self):
+         # Muestra información de la mascota y llama al método para mostrar info del dueño
         print(f"Mascota: {self.nombre}, Especie: {self.especie}, Edad: {self.edad}")
-        self.dueño.mostrar_info()
+        self.dueño.mostrar_info() # muestra los datos del dueño usando su propio método
 
     def agregar_consulta(self, motivo, fecha):
+        # Crea una nueva consulta con motivo y fecha, y la guarda en la lista
         nueva = Consulta(self, motivo, fecha)
         self.consultas.append(nueva)
 
     def mostrar_historial(self):
+        # Muestra todas las consultas que ha tenido esta mascota
         print(f"Consultas de {self.nombre}:")
         for consulta in self.consultas: 
             print(f"- {consulta.motivo} el {consulta.fecha}")
+            
+# Clase que representa una consulta veterinaria
 class Consulta:
     def __init__(self, mascota, motivo, fecha):
+        # Guardamos la mascota a la que pertenece, el motivo y la fecha de la consulta
         self.mascota = mascota
         self.motivo = motivo
         self.fecha = fecha
 
 
 # Ejemplo de como se usa
+# aqui se creo un objeto de la clase Dueño
 dueño1 = Dueño("Alex Portero", "0963002378")
+# aqui se Creo una mascota asociada a ese dueño
 mascota1 = Mascota("Mia", "Gato", 4, dueño1)
+
+# Registramos dos consultas veterinarias para la mascota
+mascota1.agregar_consulta("Vacunación", "2025-06-18")
+mascota1.agregar_consulta("Desparasitación", "2025-06-22")
 
 print("Mascota:", mascota1.nombre)
 print("Dueño:", mascota1.dueño.nombre)
 
+# aqui se muestra toda la información de la mascota y su dueño
 mascota1.mostrar_info()
+# aqui se muestra todo el historial completo de consultas
 mascota1.mostrar_historial()
